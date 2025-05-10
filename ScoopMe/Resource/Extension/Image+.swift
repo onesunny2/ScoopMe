1157ff2be4d95970a47f01699ce48d4de7454134
onesunny2 <ippne996@gmail.com>
2025-05-10 23:15:24 +0900

    Font Exension에 폰트 종류 추가, 기본적으로 사용할 basic Image, Text extension 설정

--- ScoopMe/Resource/Extension/Image+.swift
+++ ScoopMe/Resource/Extension/Image+.swift
@@ -5,4 +5,24 @@
 //  Created by Lee Wonsun on 5/10/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Image {
+    
+    // 일반 이미지
+    func basicImage(_ mode: ContentMode = .fit, width: CGFloat? = nil, height: CGFloat? = nil) -> some View {
+        self
+            .resizable()
+            .aspectRatio(contentMode: mode)
+            .frame(width: width, height: height)
+    }
+    
+    // 심볼 이미지
+    func basicImage<S: ShapeStyle>(_ mode: ContentMode = .fit, width: CGFloat? = nil, height: CGFloat? = nil, color: S) -> some View {
+        self
+            .resizable()
+            .aspectRatio(contentMode: mode)
+            .frame(width: width, height: height)
+            .foregroundStyle(color)
+    }
+}
--- ScoopMe/Resource/Extension/Text+.swift
+++ ScoopMe/Resource/Extension/Text+.swift
@@ -5,4 +5,13 @@
 //  Created by Lee Wonsun on 5/10/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension Text {
+    
+    func basicText<S: ShapeStyle>(_ font: Font, _ color: S) -> some View {
+        self
+            .font(font)
+            .foregroundStyle(color)
+    }
+}
