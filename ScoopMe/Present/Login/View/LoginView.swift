cd5ec3f641cb142537c0d7868c06b55b7b11bd27
onesunny2 <ippne996@gmail.com>
2025-05-10 23:16:46 +0900

    초기뷰 LoginView로 변경, LoginView 로직 제외 UI 구현

--- ScoopMe/Present/Login/View/LoginView.swift
+++ ScoopMe/Present/Login/View/LoginView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    private var horizontalPadding: CGFloat = 40
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.scmBrightSprout
+                .ignoresSafeArea()
+            
+            vstackContents
+        }
+    }
+    
+    private var vstackContents: some View {
+        VStack(alignment: .center, spacing: 0) {
+            
+            Text(StringLiterals.title.text)
+                .basicText(.JNLargeTitle1, .scmBlackSprout)
+            
+            Text(StringLiterals.subTitle.text)
+                .basicText(.PTTitie2, .scmBlackSprout)
+                .multilineTextAlignment(.center)
+                .padding(.top, 8)
+            
+            Image(.food3D)
+                .basicImage(width: 200)
+                .padding(.vertical, 44)
+            
+            loginButtons
+        }
+    }
+    
+    private var loginButtons: some View {
+        VStack(spacing: 10) {
+            ForEach(LoginType.allCases, id: \.rawValue) { type in
+                loginButton(type)
+            }
+        }
+    }
+    
+    private func loginButton(_ type: LoginType) -> some View {
+        
+        RoundedRectangle(cornerRadius: 5)
+            .fill(type.bgColor)
+            .overlay(alignment: .leading) {
+                type.symbolImage
+                    .basicImage(width: 18, color: type.symbolColor)
+                    .padding(.leading, 20)
+            }
+            .overlay(alignment: .center) {
+                Text(type.text)
+                    .basicText(.PTTitle3, type.labelColor)
+            }
+            .frame(height: 48)
+            .padding(.horizontal, horizontalPadding)
+    }
+}
+
+private enum StringLiterals {
+    case title
+    case subTitle
+    
+    var text: String {
+        switch self {
+        case .title: "SCOOP ME"
+        case .subTitle: "한 번의 스쿱으로\n빠르게 픽업하는 맛있는 시간"
+        }
     }
 }
 
