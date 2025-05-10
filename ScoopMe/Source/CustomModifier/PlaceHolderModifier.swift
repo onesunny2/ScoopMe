2f3ce9ce6f9fa3805d6a05cee4e1e37afee91d69
onesunny2 <ippne996@gmail.com>
2025-05-11 00:11:45 +0900

    custom placeholder modifier 생성

--- ScoopMe/Source/CustomModifier/PlaceHolderModifier.swift
+++ ScoopMe/Source/CustomModifier/PlaceHolderModifier.swift
@@ -5,4 +5,28 @@
 //  Created by Lee Wonsun on 5/10/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct PlaceHolderModifier: ViewModifier {
+    
+    let placeholder: String
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            
+            content
+            
+            if text.isEmpty {
+                Text(placeholder)
+                    .basicText(.PTBody1, .scmGray60)
+            }
+        }
+    }
+}
+
+extension View {
+    func placeholder(_ holder: String, _ text: Binding<String>) -> some View {
+        modifier(PlaceHolderModifier(placeholder: holder, text: text))
+    }
+}
