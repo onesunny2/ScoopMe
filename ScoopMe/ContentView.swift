9592786569e7cf160e3c3e643f1f79378c7cfc43
onesunny2 <ippne996@gmail.com>
2025-05-10 14:32:38 +0900

    create project and gitignore

---
+++ ScoopMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hellodddd, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoopMe/ScoopMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopMeApp.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoopMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
