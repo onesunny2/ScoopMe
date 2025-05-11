05ecf945d1d9b2ad5b82238e97398496c51c244a
onesunny2 <ippne996@gmail.com>
2025-05-11 18:28:56 +0900

    create SCMNetwork Framework

---
+++ SCMNetwork/SCMNetwork/SCMNetwork.h
@@ -0,0 +1,18 @@
+//
+//  SCMNetwork.h
+//  SCMNetwork
+//
+//  Created by Lee Wonsun on 5/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCMNetwork.
+FOUNDATION_EXPORT double SCMNetworkVersionNumber;
+
+//! Project version string for SCMNetwork.
+FOUNDATION_EXPORT const unsigned char SCMNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCMNetwork/PublicHeader.h>
+
+
