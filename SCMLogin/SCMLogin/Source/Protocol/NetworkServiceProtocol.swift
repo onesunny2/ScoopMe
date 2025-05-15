df72ea57c52a3812d733eb5545ad7fb420a0dd57
onesunny2 <ippne996@gmail.com>
2025-05-15 21:59:55 +0900

    기존의 UserServiceProtocol의 기능 중 네트워크 통신만 필요로 하는 매니저를 고려해 NetworkServiceProtocol을 만들어 기존의 프로토콜은 network 프로토콜을 채택하도록 수정

--- SCMLogin/SCMLogin/Source/Protocol/NetworkServiceProtocol.swift
+++ SCMLogin/SCMLogin/Source/Protocol/NetworkServiceProtocol.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+internal import SCMNetwork
+
+protocol NetworkServiceProtocol: AnyObject {
+    var network: SCMNetworkImpl { get }
+    
+    func callRequest<T: Decodable>(_ value: LoginURL, type: T.Type) async throws -> HTTPResponse<T>
+}
+
+extension NetworkServiceProtocol {
+    
+    func callRequest<T: Decodable>(_ value: LoginURL, type: T.Type) async throws -> HTTPResponse<T> {
+        let request = HTTPRequest(
+            scheme: .http,
+            method: value.method,
+            successCodes: [200]
+        )
+            .addBaseURL(value.baseURL)
+            .addPath(value.path)
+            .addParamters(value.parameters)
+            .addHeaders(value.headers)
+        
+        return try await network.fetchData(request, T.self)
+    }
+}
--- SCMLogin/SCMLogin/Source/Protocol/UserServiceProtocol.swift
+++ SCMLogin/SCMLogin/Source/Protocol/UserServiceProtocol.swift
@@ -9,30 +9,13 @@ import Foundation
 import Combine
 internal import SCMNetwork
 
-protocol UserServiceProtocol: ObservableObject, AnyObject {
+protocol UserServiceProtocol: NetworkServiceProtocol, ObservableObject {
     
     var alertTitle: String { get set }
     var alertMessage: String { get set }
-    var network: SCMNetworkImpl { get }
-    
-    func callRequest<T: Decodable>(_ value: LoginURL, method: HTTPMethods, type: T.Type) async throws -> HTTPResponse<T>
 }
 
 extension UserServiceProtocol {
-    
-    func callRequest<T: Decodable>(_ value: LoginURL, method: HTTPMethods, type: T.Type) async throws -> HTTPResponse<T> {
-        let request = HTTPRequest(
-            scheme: .http,
-            method: method,
-            successCodes: [200]
-        )
-            .addBaseURL(value.baseURL)
-            .addPath(value.path)
-            .addParamters(value.parameters)
-            .addHeaders(value.headers)
-        
-        return try await network.fetchData(request, T.self)
-    }
     
     @MainActor
     func handleError(_ error: Error, _ alertMessage: inout String) {
