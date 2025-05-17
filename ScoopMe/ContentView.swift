0fec09d49037eb8237065a7310899a2943835d89
onesunny2 <ippne996@gmail.com>
2025-05-17 17:04:33 +0900

    ContentView에 MainFlow에 따라 화면 분기처리 되도록 구성

--- ScoopMe/ContentView.swift
+++ ScoopMe/ContentView.swift
@@ -8,45 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var router = SCMRouter<MainFlow>.shared
+    @State private var currentFlow: MainFlow = .splash
+    
     var body: some View {
-        VStack {
-            Image(.run)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50)
-                .foregroundStyle(.scmBrightForsythia)
-            
-            Text("JNLargeTitle1")
-                .font(.JNLargeTitle1)
-            Text("JNLargeTitle2")
-                .font(.JNLargeTitle2)
-            Text("JNTitle1")
-                .font(.JNTitle1)
-            Text("JNBody1")
-                .font(.JNBody1)
-            Text("JNCaption1")
-                .font(.JNCaption1)
-            
-            Divider()
-            
-            Text("PTTitle1")
-                .font(.PTTitle1)
-            Text("PTTitle2")
-                .font(.PTTitie2)
-            Text("PTBody1")
-                .font(.PTBody1)
-            Text("PTBody2")
-                .font(.PTBody2)
-            Text("PTBody3")
-                .font(.PTBody3)
-            Text("PTCaption1")
-                .font(.PTCaption1)
-            Text("PTCaption2")
-                .font(.PTCaption2)
-            Text("PTCaption3")
-                .font(.PTCaption3)
+        mainFlowView
+    }
+    
+    @ViewBuilder
+    private var mainFlowView: some View {
+        switch currentFlow {
+        case .splash:
+            SplashView()
+        case let .login(manager):
+            LoginView(loginManager: manager)
+        case .main:
+            MainTabView()
         }
-        .padding()
     }
 }
 
--- ScoopMe/Source/NavigationRouter/Path/MainFlow.swift
+++ ScoopMe/Source/NavigationRouter/Path/MainFlow.swift
@@ -6,9 +6,10 @@
 //
 
 import Foundation
+import SCMLogin
 
 enum MainFlow: Hashable {
     case splash
-    case login
+    case login(LoginManager)
     case main
 }
