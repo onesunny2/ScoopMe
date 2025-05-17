9ce34a355d820e155c764469a330e956917d06b4
onesunny2 <ippne996@gmail.com>
2025-05-17 16:00:18 +0900

    기존 NavigationRouter에서 추상화된 Router<P>로 변경, 큰 흐름을 공유하는 화면끼리 같은 화면 전환 Flow에 있도록 Path 구조 변경

---
+++ ScoopMe/Source/NavigationRouter/Path/MainFlow.swift
@@ -0,0 +1,14 @@
+//
+//  MainFlow.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/16/25.
+//
+
+import Foundation
+
+enum MainFlow: Hashable {
+    case splash
+    case login
+    case main
+}
--- ScoopMe/Source/NavigationRouter/Router.swift
+++ ScoopMe/Source/NavigationRouter/Router.swift
@@ -1,78 +1,49 @@
 //
-//  Router.swift
+//  Navigation.swift
 //  ScoopMe
 //
-//  Created by Lee Wonsun on 5/11/25.
+//  Created by Lee Wonsun on 5/16/25.
 //
 
-import SwiftUI
+import Foundation
 
-final class Router: ObservableObject {
+final class Router<P: Hashable>: ObservableObject {
+    private init() { }
     
-    // 앱 전체 네비게이션
-    @Published var appPath = NavigationPath()
+    @Published var path = Array<P>()
     
-    // 각 최상위 화면별 네비게이션
-    @Published var loginPath = NavigationPath()
+    func send(_ action: Action) {
+        action(action)
+    }
     
-    // 현재 선택된 탭
-    @Published var selectedTab: Int = 0
-    
-    // 로그인 여부
-    @Published var isLoggedIn: Bool = false
-    
-    // 선택된 탭으로 이동
-    // 만약 각 탭으로 이동했을 때 기존의 화면을 root로 돌리고 싶으면 작업하기
-    
-    // 앱 전체 수준의 화면 전환 관리
-    func pushToAppRoute(_ route: AppRoute) {
-        appPath.append(route)
-        
-        // 탭 간의 이동일 경우 selectedTab 업데이트
-        switch route {
-        case .home(let tab): selectedTab = tab
-        case .order(let tab): selectedTab = tab
-        case .community(let tab): selectedTab = tab
-        case .profile(let tab): selectedTab = tab
-        case .chatting(let tab): selectedTab = tab
-        default: break
+    private func action(_ action: Router<P>.Action) {
+        switch action {
+        case .push(let view):
+            path.append(view)
+        case .pop:
+            path.removeLast()
+        case .pops(let count):
+            let removeCount = min(count, path.count)
+            path.removeLast(count)
+        case .popAll:
+            path.removeAll()
         }
     }
 }
 
-// 로그인 완료, 로그아웃 관련
-extension Router {
-    
-    func completeLogin() {
-        isLoggedIn = true
-        loginPath = NavigationPath()
-        
-        // 홈 탭화면으로 이동
-        pushToAppRoute(.home(tab: 0))
-    }
-    
-    func logout() {
-        isLoggedIn = false
-        
-        // 모든 경로 초기화
-        appPath = NavigationPath()
-        loginPath = NavigationPath()
-    }
+extension Router where Router == Router<LoginPath> {
+    static let shared = Router()
 }
 
-// 로그인 화면 관련
+extension Router where Router == Router<MainFlow> {
+    static let shared = Router()
+}
+
 extension Router {
-    
-    func pushLoginRoute(_ route: LoginRoute) {
-        loginPath.append(route)
-    }
-    
-    func popLoginRoute() {
-        guard !loginPath.isEmpty else { return }
-        loginPath.removeLast()
-    }
-    
-    func popToLoginRoot() {
-        loginPath = NavigationPath()
+    enum Action {
+        case push(P)
+        case pop
+        case pops(Int)
+        case popAll
     }
 }
