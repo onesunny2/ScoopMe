23c46c83a701937d8de91038be9972b30a92db6f
onesunny2 <ippne996@gmail.com>
2025-05-18 15:00:37 +0900

    DIContainer 구현

---
+++ ScoopMe/APP/DIContainer.swift
@@ -0,0 +1,46 @@
+//
+//  DIContainer.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/18/25.
+//
+
+import Foundation
+import SCMLogger
+import SCMLogin
+
+final class DIContainer {
+    static var shared = DIContainer()
+    
+    private var services: [ObjectIdentifier: () -> AnyObject]
+    
+    private init() {
+        self.services = [:]
+        
+        register(LoginManager.self) { LoginManager() }
+        register(LoginTokenManager.self) { LoginTokenManager() }
+    }
+    
+    func register<T: AnyObject>(
+        _ type: T.Type,
+        factory: @escaping () -> T
+    ) {
+        let key = ObjectIdentifier(type)
+        
+        if services[key] != nil {
+            Log.error("DIContainer: \(type) is already registered. Overwriting...")
+        }
+        
+        services[key] = factory
+    }
+    
+    func resolve<T: AnyObject>(_ type: T.Type) -> T {
+        let key = ObjectIdentifier(type)
+        guard let factory = services[key] as? () -> T else {
+            fatalError("DIContainer: \(type) not registered")
+            Log.error("DIContainer: \(type) not registered")
+        }
+        
+        return factory()
+    }
+}
---
+++ ScoopMe/Source/Utilities/PropertyWrapper/Dependency.swift
@@ -0,0 +1,8 @@
+//
+//  Dependency.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/18/25.
+//
+
+import Foundation
