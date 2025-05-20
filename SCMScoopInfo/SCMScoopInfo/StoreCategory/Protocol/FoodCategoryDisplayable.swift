071dc87f0316f6752dfb867a8f2841399aca5c42
onesunny2 <ippne996@gmail.com>
2025-05-20 20:42:39 +0900

    enum, protocol 폴더링 변경

---
+++ SCMScoopInfo/SCMScoopInfo/StoreCategory/Protocol/FoodCategoryDisplayable.swift
@@ -0,0 +1,24 @@
+//
+//  FoodCategoryDisplayable.swift
+//  SCMScoopInfo
+//
+//  Created by Lee Wonsun on 5/20/25.
+//
+
+import SwiftUI
+import Combine
+
+public protocol FoodCategoryDisplayable: AnyObject, ObservableObject {
+    
+    var categoryNames: [String] { get set }
+    var categoryImages: [Image] { get set }
+    
+    var selectedCategory: String { get set }
+    
+    func getPopularKeywords() async -> PopularSearchKeywordEntity
+    func getPopularStoresInfo() async -> [RealtimePopularScoopEntity]
+    func getAroundStoreInfo(
+        _ round: AroundType,
+        _ filter: AroundFilterType
+    ) async -> [AroundStoreInfoEntity]
+}
