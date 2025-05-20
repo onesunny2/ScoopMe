b08606e01ebfc8c8ea00a3caeeb6b9a292bb870b
onesunny2 <ippne996@gmail.com>
2025-05-20 15:58:20 +0900

    splashView 3Dimage 교체

---
+++ ScoopMe/Present/Main/Home/RealtimePopularScoopCell.swift
@@ -0,0 +1,18 @@
+//
+//  RealtimePopularScoopCell.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 5/20/25.
+//
+
+import SwiftUI
+
+struct RealtimePopularScoopCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RealtimePopularScoopCell()
+}
