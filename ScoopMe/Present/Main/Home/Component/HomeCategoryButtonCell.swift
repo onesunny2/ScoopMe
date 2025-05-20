48801d0eb7ae696f5cd8169badd7ec6943ae7d30
onesunny2 <ippne996@gmail.com>
2025-05-20 11:30:50 +0900

    카테고리 메뉴 버튼을 위한 버튼 셀 구현

--- ScoopMe/Present/Main/Home/Component/HomeCategoryButtonCell.swift
+++ ScoopMe/Present/Main/Home/Component/HomeCategoryButtonCell.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct HomeCategoryButtonCell: View {
+    
+    let image: Image
+    let title: String
+    let isSelected: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 4) {
+            rectangleImage
+            categoryText
+        }
+    }
+    
+    private var rectangleImage: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(.scmGray0)
+            .frame(width: 56, height: 56)
+            .overlay(alignment: .center) {
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(
+                        isSelected ? .scmBlackSprout : .scmGray45,
+                        lineWidth: isSelected ? 2 : 1
+                    )
+                    .frame(width: 56, height: 56)
+            }
+            .overlay(alignment: .center) {
+                image
+                    .basicImage(width: 32)
+            }
+    }
+    
+    private var categoryText: some View {
+        Text(title)
+            .basicText(
+                isSelected ? .PTBody3 : .PTBody4,
+                isSelected ? .scmBlackSprout :  .scmGray60
+            )
     }
 }
 
 #Preview {
-    HomeCategoryButtonCell()
+    HomeCategoryButtonCell(image: Image(.fastFood), title: "패스트푸드", isSelected: false)
 }
