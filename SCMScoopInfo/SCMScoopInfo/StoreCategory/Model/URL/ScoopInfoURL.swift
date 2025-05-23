874b75708aa59df8e1e085380cbc6ad883ab56b8
onesunny2 <ippne996@gmail.com>
2025-05-23 17:59:26 +0900

    위치기반 주변 가게 검색 API 2개로 분기처리 했던 부분 1개로 통일

--- SCMScoopInfo/SCMScoopInfo/StoreCategory/Model/URL/ScoopInfoURL.swift
+++ SCMScoopInfo/SCMScoopInfo/StoreCategory/Model/URL/ScoopInfoURL.swift
@@ -9,8 +9,7 @@ import Foundation
 internal import SCMNetwork
 
 public enum ScoopInfoURL {
-    case nearbyStoreFirst(access: String, category: String, longitude: Float, latitude: Float, limit: Int, orderBy: AroundFilterType)
-    case nearbyStoreNext(access: String, category: String, longitude: Float, latitude: Float, next: String, limit: Int, orderBy: AroundFilterType)
+    case nearbyStore(access: String, category: String, longitude: Float, latitude: Float, next: String, limit: Int, orderBy: AroundFilterType)
     case popularKeyword(access: String)
     case realtimePopularStores(access: String, category: Category)
     case postStoreLike(access: String, storeID: String, status: Bool)
@@ -30,7 +29,7 @@ public enum ScoopInfoURL {
     
     var path: String {
         switch self {
-        case .nearbyStoreFirst, .nearbyStoreNext: "/v1/stores"
+        case .nearbyStore: "/v1/stores"
         case .popularKeyword: "/v1/stores/searches-popular"
         case .realtimePopularStores: "/v1/stores/popular-stores"
         case let .postStoreLike(_, storeID, _): "/v1/stores/\(storeID)/like"
@@ -39,15 +38,7 @@ public enum ScoopInfoURL {
     
     var parameters: [String: String?]? {
         switch self {
-        case let .nearbyStoreFirst(_, category, longitude, latitude, limit, orderBy):
-            return [
-                "category": category,
-                "longitude": "\(longitude)",
-                "latitude": "\(latitude)",
-                "limit": "\(limit)",
-                "order_by": orderBy.query
-            ]
-        case let .nearbyStoreNext(_, category, longitude, latitude, next, limit, orderBy):
+        case let .nearbyStore(_, category, longitude, latitude, next, limit, orderBy):
             return [
                 "category": category,
                 "longitude": "\(longitude)",
@@ -72,13 +63,7 @@ public enum ScoopInfoURL {
     
     var headers: [String: String] {
         switch self {
-        case let .nearbyStoreFirst(access, _, _, _, _, _):
-            return [
-                "Content-Type": "application/json",
-                "SeSACKey": Secret.apiKey,
-                "Authorization": access
-            ]
-        case let .nearbyStoreNext(access, _, _, _, _, _, _):
+        case let .nearbyStore(access, _, _, _, _, _, _):
             return [
                 "Content-Type": "application/json",
                 "SeSACKey": Secret.apiKey,
