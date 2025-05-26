d97411fdad42c0822f154d241f6c1ec15d83fec6
onesunny2 <ippne996@gmail.com>
2025-05-26 11:55:42 +0900

    SCMScoopInfo 모듈의 폴더링 재구성

---
+++ SCMScoopInfo/SCMScoopInfo/Repository/Mock/MockStoreDetailRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockStoreDetailRepository.swift
+//  SCMScoopInfo
+//
+//  Created by Lee Wonsun on 5/26/25.
+//
+
+import Foundation
---
+++ SCMScoopInfo/SCMScoopInfo/Source/Protocol/StoreDetailDisplayable.swift
@@ -0,0 +1,13 @@
+//
+//  StoreDetailDisplayable.swift
+//  SCMScoopInfo
+//
+//  Created by Lee Wonsun on 5/26/25.
+//
+
+import SwiftUI
+import Combine
+
+protocol StoreDetailDisplayable: AnyObject, ObservableObject {
+    
+}
