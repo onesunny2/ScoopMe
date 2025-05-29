4d809f75fc3ec39f32aca68876ce1797dc61812b
onesunny2 <ippne996@gmail.com>
2025-05-29 19:04:38 +0900

    homeView에서 사용된 searchTextFieldCell 재사용하여 CommunityView에 적용

--- ScoopMe/Present/Main/Community/View/CommunityView.swift
+++ ScoopMe/Present/Main/Community/View/CommunityView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct CommunityView: View {
     
+    @State private var searchKeyword: String = ""
     @State private var volume: CGFloat = 0.3
     
     var body: some View {
         NavigationStack {
-            VStack {
+            LazyVStack(spacing: 20) {
+                SearchTextFieldCell(
+                    placeholder: StringLiterals.placeholder.text,
+                    keyword: $searchKeyword
+                )
+                
                 ExpendableSliderCell(value: $volume, in: 0...1) {
                     HStack {
                         Image(.distance)
@@ -26,11 +32,21 @@ struct CommunityView: View {
                     }
                     .defaultHorizontalPadding()
                 }
-                .defaultHorizontalPadding()
             }
-            .navigationTitle("스쿱미 커뮤니티")
+            .defaultHorizontalPadding()
+            .navigationTitle(StringLiterals.navigationTitle.text)
             .navigationBarTitleDisplayMode(.inline)
         }
+    }
+}
+
+// MARK: StringLiterals
+private enum StringLiterals: String {
+    case navigationTitle = "스쿱미 소식통"
+    case placeholder = "검색어를 입력해주세요."
+    
+    var text: String {
+        return self.rawValue
     }
 }
 
