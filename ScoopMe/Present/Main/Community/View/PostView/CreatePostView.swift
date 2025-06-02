d027c54cc3e7ea3beb2840168769199a7903ac64
onesunny2 <ippne996@gmail.com>
2025-06-02 09:24:59 +0900

    add toolbarItem

--- ScoopMe/Present/Main/Community/View/PostView/CreatePostView.swift
+++ ScoopMe/Present/Main/Community/View/PostView/CreatePostView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct CreatePostView: View {
     var body: some View {
-        Text("CreatePostView")
+        NavigationStack {
+            Text("CreatePostView")
+                .navigationTitle(StringLiterals.navigationTitle.text)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarItem {
+                    Text(StringLiterals.close.text)
+                        .basicText(.PTBody1, .scmGray90)
+                } trailing: {
+                    Text(StringLiterals.upload.text)
+                        .basicText(.PTBody1, .scmGray90)
+                }
+        }
+    }
+}
+
+// MARK: String Literals
+private enum StringLiterals: String {
+    case navigationTitle = "작성하기"
+    case close = "닫기"
+    case upload = "올리기"
+    
+    var text: String {
+        return self.rawValue
     }
 }
 
