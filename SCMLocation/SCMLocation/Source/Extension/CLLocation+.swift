596b6a9ce799ba6f302ef0ae6122783fd65541d5
onesunny2 <ippne996@gmail.com>
2025-06-09 09:36:24 +0900

    커뮤니티 포스트 화면에 사용될 거리, 날짜 계산을 위한 extension 추가

--- SCMLocation/SCMLocation/Source/Extension/CLLocation+.swift
+++ SCMLocation/SCMLocation/Source/Extension/CLLocation+.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import CoreLocation
+
+public extension CLLocation {
+    func distanceInKm(from location: CLLocation) -> String {
+        let distance = self.distance(from: location) / 1000.0
+        return String(format: "%.2f", distance) + "km"
+    }
+    
+    func intDistanceInM(from location: CLLocation) -> Int {
+        let distance = self.distance(from: location)
+        return Int(distance.rounded())
+    }
+}
--- SCMLocation/SCMLocation/Source/Extension/UserDefaults+.swift
+++ SCMLocation/SCMLocation/Source/Extension/UserDefaults+.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+public extension UserDefaults {
+    
+    func saveStructArray<T: Codable>(_ value: T, for key: String) {
+        
+        var values = self.fetchStruct([T].self, for: key)
+        values?.append(value)
+        
+        if let data = try? Coder.encoder.encode(values) {
+            self.set(data, forKey: key)
+        }
+    }
+    
+    func deleteStructArray<T: Codable & Equatable>(_ value: T, for key: String) {
+        
+        let values = self.fetchStruct([T].self, for: key)
+        guard let values, !values.isEmpty else { return }
+        
+        let index = values.firstIndex { $0 == value }
+        guard let index else { return }
+        
+        var arrays = values
+        arrays.remove(at: index)
+        
+        if let data = try? Coder.encoder.encode(arrays) {
+            self.set(data, forKey: key)
+        }
+    }
+    
+    func fetchStruct<T: Decodable>(_ type: T.Type, for key: String) -> T? {
+        
+        guard let data = self.data(forKey: key) else { return nil }
+        guard let decoded = try? Coder.decoder.decode(T.self, from: data) else { return nil}
+        
+        return decoded
+    }
+}
