82ecec77a26358bfaba949ebde1799970cf05b9b
onesunny2 <ippne996@gmail.com>
2025-06-19 20:46:38 +0900

    socketIO SPM 설치, SCMChat 모듈 생성

---
+++ SCMChat/SCMChat/SCMChat.h
@@ -0,0 +1,18 @@
+//
+//  SCMChat.h
+//  SCMChat
+//
+//  Created by Lee Wonsun on 6/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCMChat.
+FOUNDATION_EXPORT double SCMChatVersionNumber;
+
+//! Project version string for SCMChat.
+FOUNDATION_EXPORT const unsigned char SCMChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCMChat/PublicHeader.h>
+
+
