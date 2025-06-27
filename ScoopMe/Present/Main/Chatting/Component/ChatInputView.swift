ec9bde1fbf44fe4a9b4d074df5b7b7e050d8b7de
onesunny2 <ippne996@gmail.com>
2025-06-27 23:48:53 +0900

    chatInputView 생성

---
+++ ScoopMe/Present/Main/Chatting/Component/ChatInputView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatInputView.swift
+//  ScoopMe
+//
+//  Created by Lee Wonsun on 6/27/25.
+//
+
+import SwiftUI
+
+struct ChatInputView: View {
+    var body: some View {
+        Text("ChatInputView")
+    }
+}
+
+#Preview {
+    ChatInputView()
+}
