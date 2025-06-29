7e73720bea1ea7a9e7fb8656377abca31cebbe66
onesunny2 <ippne996@gmail.com>
2025-06-29 20:04:39 +0900

    채팅방 생성, 채팅방 메시지 조회에 관한 chatURL case 추가

--- SCMChat/SCMChat/Data/URL/ChatURL.swift
+++ SCMChat/SCMChat/Data/URL/ChatURL.swift
@@ -9,5 +9,61 @@ import Foundation
 import SCMNetwork
 
 public enum ChatURL {
+    case fetchChatRoom(access: String, opponentUserid: String)
+    case loadChatRoom(access: String)
     
+    var baseURL: String {
+        return Secret.baseURL
+    }
+    
+    var method: HTTPMethods {
+        switch self {
+        case .fetchChatRoom:
+            return .post
+        case .loadChatRoom:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchChatRoom, .loadChatRoom:
+            return "/v1/chats"
+        }
+    }
+    
+    var parameters: [String: String?]? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+    
+    var jsonBody: [String: Any?]? {
+        switch self {
+        case let .fetchChatRoom(_, opponentUserid):
+            return [
+                "opponent_id": opponentUserid
+            ]
+        default:
+            return nil
+        }
+    }
+    
+    var headers: [String: String] {
+        switch self {
+        case let .fetchChatRoom(access, _):
+            return [
+                "Content-Type": "application/json",
+                "SeSACKey": Secret.apiKey,
+                "Authorization": access
+            ]
+        case let .loadChatRoom(access):
+            return [
+                "Content-Type": "application/json",
+                "SeSACKey": Secret.apiKey,
+                "Authorization": access
+            ]
+        }
+    }
 }
